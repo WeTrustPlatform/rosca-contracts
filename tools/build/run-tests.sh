233ac3747a1c46e2fc485d35c4bd4fa42452e9ad
Ron Merom <rmerom@gmail.com>
2016-12-20 09:00:18 -0800

    upgrade run-tests.sh script (#30)

--- tools/build/run-tests.sh
+++ tools/build/run-tests.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+# Creates the ROSCATest.sol contract (with members publicized)
+# and runs the tests.
+# Any commandline parameters are passed to "truffle test".
+
 runCommand() {
   echo "### $@"
-  $@
+  $@ || { echo "*** Command '$@' failed, exiting" ; exit 1; }
 }
+
 # Create ROSCATest.sol with all the internal
 # variable and functions publicized
 runCommand ./tools/build/publicizer.py contracts/ROSCA.sol
 
 runCommand truffle compile
 runCommand truffle migrate --reset
-runCommand truffle test
+runCommand truffle test $@
