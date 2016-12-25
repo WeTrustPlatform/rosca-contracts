8d12e7dec1a28cd33209df78d788cf70da0a8790
Ron Merom <rmerom@gmail.com>
2016-12-24 19:46:00 -0800

    first stab at travis (#42)

---
+++ tools/build/run-testrpc-and-tests.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+../../node_modules/ethereumjs-testrpc/bin/testrpc -m "myth like bonus scare over problem client lizard pioneer submit female collect" &
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+$DIR/run-tests.sh
