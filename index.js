dd68a3cdad9e023005701cc104dd4a032a79d37e
Ron Merom <ron@wetrust.io>
2017-01-04 06:49:29 +0000

    fixes based on an's review

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-var fs = require('fs');
+let fs = require('fs');
 let solc = require('solc');
 
 let contractCache;
-let contractErr;
 
 let WeTrustContract = {
   /**
    * Call this function upon server start. It reads and compiles the contract, and saves it in a cache variable.
    * Throws if there was an error.
    */
-  init: function() {
+  init: function(done) {
     let roscaContractLocation = 'contracts/ROSCA.sol';
 
     fs.readFile(roscaContractLocation, 'utf8', function(err, contractCode) {
       if (err) {
-        contractErr = err;
-        throw Error(err);
+        if (done) {
+          done(err);
+        }
       } else {
         let output = solc.compile(contractCode, 1);
         let roscaContractAbi = JSON.parse(output.contracts.ROSCA.interface);
         let bytecode = output.contracts.ROSCA.bytecode;
         contractCache = {abi: roscaContractAbi, bytecode: bytecode};
+        if (done) {
+          done();
+        }
       }
     });
   },
@@ -32,16 +35,12 @@ let WeTrustContract = {
    *   bytecode: the bytecode of the contract.
    * }
    */
-  getContract: function(done) {
-    if (!contractCache && !contractErr) {
-      throw Error("must call wetrust-rosca-contract's init() first");
+  getContract: function() {
+    if (!contractCache) {
+      throw Error("must call wetrust-rosca-contract's init() first and let is complete");
     }
-    if (contractErr) {
-      done(contractErr);
-    } else {
-      done(undefined, contractCache);
-    }
-  }
+    return contractCache;
+  },
 };
 
 module.exports = WeTrustContract;
